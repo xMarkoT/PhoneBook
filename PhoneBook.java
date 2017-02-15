5dcbb4bee6ba6ef306dc9b6fb919ac17de4c7bae
marko.trajcheski <marko.trajcheski@musala.com>
2017-02-15 20:01:11 +0100

    modified

--- PhoneBook.java
+++ PhoneBook.java
@@ -23,30 +23,94 @@ import java.util.Set;
  */
 
 public class PhoneBook {
+	
+	private final String SEPPARATOR = ", ";
+	private final List<String> OPERATOR_CODES = Arrays.asList("87", "88", "89");
+	private final List<Character> SPECIAL_CHAR = Arrays.asList('2', '3', '4', '5', '6', '7', '8', '9');
 
-	private final static String SEPPARATOR = ", ";
-	private final static List<String> OPERATOR_CODES = Arrays.asList("87", "88", "89");
-	private final static List<Character> SPECIAL_CHAR = Arrays.asList('2', '3', '4', '5', '6', '7', '8', '9');
-
-	private static Map<String, String> nameToPhone = new HashMap<>();
-	private static Map<String, Integer> phoneToCall = new HashMap<>();
+	private Map<String, String> nameToPhone = new HashMap<>();
+	private Map<String, Integer> phoneToCall = new HashMap<>();
 
 	private static String rawFile;
 	private static String parsedFile;
 	
 
-	private static boolean validatePhone(String phone) {
-		boolean p = phone.startsWith("+");
-		phone = phone.replaceAll("\\D+", "");
-		return (p && phone.length() == 12 && phone.startsWith("359") && OPERATOR_CODES.contains(phone.substring(3, 5))
-				&& SPECIAL_CHAR.contains(phone.charAt(5)))
-				|| (!p && phone.length() == 10 && '0' == phone.charAt(0)
-						&& OPERATOR_CODES.contains(phone.substring(1, 3)) && SPECIAL_CHAR.contains(phone.charAt(3)))
-				|| (!p && phone.length() == 14 && phone.startsWith("00") && "359".equals(phone.substring(2, 5))
-						&& OPERATOR_CODES.contains(phone.substring(5, 7)) && SPECIAL_CHAR.contains(phone.charAt(7)));
+	public String getPhoneNumberByName(String name) {
+		return nameToPhone.get(name);
 	}
 
-	public static void parseRawFile(String rawFile, String parsedFile) {
+	public void removeNumber(String parsedFile, String name) {
+		phoneBookDataOperatoins(parsedFile, name, null, true);
+	}
+
+	public void addNumber(String parsedFile, String name, String number) {
+		phoneBookDataOperatoins(parsedFile, name, number, false);
+	}
+	
+	public void printSortedMapByName() {
+		List<String> keys = new ArrayList<>(nameToPhone.keySet());
+		keys.sort(new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		for (String key : keys) {
+			System.out.println(key + ", " + nameToPhone.get(key));
+		}
+	}
+	
+	public void printCallStatistic() {
+		Set<Entry<String, Integer>> set = phoneToCall.entrySet();
+		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		int limit = 5;
+		for (Map.Entry<String, Integer> entry : list) {
+			if (limit <= 0) {
+				break;
+			}
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+			limit--;
+		}
+	}
+	
+	private String validatePhone(String phone) {
+		boolean p = phone.toString().startsWith("+");
+		phone = phone .replaceAll("\\D+", "");
+		
+		if(p && phone.length() == 12 && phone.startsWith("359") && OPERATOR_CODES.contains(phone.substring(3, 5))
+				&& SPECIAL_CHAR.contains(phone.charAt(5))){
+			return phone;
+		}
+		
+		if(!p && phone.length() == 10 && '0' == phone.charAt(0)
+				&& OPERATOR_CODES.contains(phone.substring(1, 3)) && SPECIAL_CHAR.contains(phone.charAt(3))){
+			
+			return "+359"+phone.substring(1);
+		}
+		
+		if(!p && phone.length() == 14 && phone.startsWith("00") && "359".equals(phone.substring(2, 5))
+				&& OPERATOR_CODES.contains(phone.substring(5, 7)) && SPECIAL_CHAR.contains(phone.charAt(7))){
+			return "+" + phone.substring(2);
+		}
+		
+//		return (p && phone.length() == 12 && phone.startsWith("359") && OPERATOR_CODES.contains(phone.substring(3, 5))
+//				&& SPECIAL_CHAR.contains(phone.charAt(5)))
+//				|| (!p && phone.length() == 10 && '0' == phone.charAt(0)
+//						&& OPERATOR_CODES.contains(phone.substring(1, 3)) && SPECIAL_CHAR.contains(phone.charAt(3)))
+//				|| (!p && phone.length() == 14 && phone.startsWith("00") && "359".equals(phone.substring(2, 5))
+//						&& OPERATOR_CODES.contains(phone.substring(5, 7)) && SPECIAL_CHAR.contains(phone.charAt(7)));
+		return null;
+	}
+	
+
+	public void parseRawFile(String rawFile, String parsedFile) {
 		BufferedReader br = null;
 		BufferedWriter bw = null;
 		try {
@@ -58,9 +122,9 @@ public class PhoneBook {
 				if (split.length > 1) {
 					String name = split[0];
 					String phone = split[1];
-					if (validatePhone(phone)) {
-						nameToPhone.put(name, phone);
-						phoneToCall.put(name, 0);
+					if (validatePhone(phone) != null) {
+						nameToPhone.put(name, validatePhone(phone));
+						phoneToCall.put(validatePhone(phone), 0);
 						bw.write(name + SEPPARATOR + phone);
 						bw.newLine();
 						bw.flush();
@@ -91,20 +155,8 @@ public class PhoneBook {
 			}
 		}
 	}
-	
-	public static String getPhoneNumberByName(String name) {
-		return nameToPhone.get(name);
-	}
 
-	public static void removeNumber(String parsedFile, String name) {
-		phoneBookDataOperatoins(parsedFile, name, null, true);
-	}
-
-	public static void addNumber(String parsedFile, String name, String number) {
-		phoneBookDataOperatoins(parsedFile, name, number, false);
-	}
-
-	private static void phoneBookDataOperatoins(String parsedFile, String name, String number, boolean removeRecord) {
+	private void phoneBookDataOperatoins(String parsedFile, String name, String number, boolean removeRecord) {
 
 		BufferedReader br = null;
 		BufferedWriter bw = null;
@@ -123,11 +175,11 @@ public class PhoneBook {
 				sb.append(line);
 				sb.append("\r\n");
 			}
-			if (removeRecord == false && validatePhone(number)) {
-				sb.append(name + SEPPARATOR + number);
+			if (removeRecord == false && validatePhone(number) != null) {
+				sb.append(name + SEPPARATOR + validatePhone(number));
 				sb.append("\r\n");
 				nameToPhone.put(name, number);
-				phoneToCall.put(name, 0);
+				phoneToCall.put(validatePhone(number), 0);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -172,38 +224,9 @@ public class PhoneBook {
 		}
 	}
 
-	public static void printCallStatistic() {
-		Set<Entry<String, Integer>> set = phoneToCall.entrySet();
-		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
-		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-				return (o2.getValue()).compareTo(o1.getValue());
-			}
-		});
+	
 
-		int limit = 5;
-		for (Map.Entry<String, Integer> entry : list) {
-			if (limit <= 0) {
-				break;
-			}
-			System.out.println(entry.getKey() + " : " + entry.getValue());
-			limit--;
-		}
-	}
-
-	public static void printSortedMapByName() {
-		List<String> keys = new ArrayList<>(nameToPhone.keySet());
-		keys.sort(new Comparator<String>() {
-
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.compareTo(o2);
-			}
-		});
-		for (String key : keys) {
-			System.out.println(key + ", " + nameToPhone.get(key));
-		}
-	}
+	
 
 	public String getRawFile() {
 		return rawFile;
