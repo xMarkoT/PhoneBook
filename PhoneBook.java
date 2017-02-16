175f83ddfc2aaeecab84fb754d4c24e292d42763
marko.trajcheski <marko.trajcheski@musala.com>
2017-02-16 14:02:06 +0100

    formatted

--- PhoneBook.java
+++ PhoneBook.java
@@ -34,18 +34,44 @@ public class PhoneBook {
 
 	private String rawFilePath;
 	private String parsedFilePath;
-	
+
+	public static void main(String[] args) {
+		String rawFilePath = "C:\\Users\\marko.trajcheski\\Desktop\\PhoneBook\\imenik.txt";
+		String parsedFilePath = "C:\\Users\\marko.trajcheski\\Desktop\\PhoneBook\\parsedImenik.txt";
+		PhoneBook pb = new PhoneBook(rawFilePath, parsedFilePath);
+
+		pb.parseRawFile();
+
+		// pb.printSortedMapByName();
+
+		pb.addPhoneRecordToFile("REC1", "0888111111");
+		pb.addPhoneRecordToFile("REC2", "00359888111111");
+		pb.addPhoneRecordToFile("REC3", "+359888111111");
+		pb.addPhoneRecordToFile("REC4", "0881111111");
+		pb.addPhoneRecordToFile("REC5", "+355888111111");
+		pb.addPhoneRecordToFile("REC6", "0888111111");
+		pb.addPhoneRecordToFile("REC7", "0888111111");
+		pb.addPhoneRecordToFile("REC8", "0888111111");
+
+		// pb.printSortedMapByName();
+
+		pb.removePhoneRecord("REC8");
+		pb.removePhoneRecord("REC1");
+
+		pb.printSortedMapByName();
+	}
+
 	public PhoneBook(String rawFilePath, String parsedFilePath) {
 		this.rawFilePath = rawFilePath;
 		this.parsedFilePath = parsedFilePath;
 	}
 
 	public String getPhoneNumberByName(String name) {
-		return nameToPhone.get(name);
+		return getNameToPhone().get(name);
 	}
 
 	public void printSortedMapByName() {
-		List<String> keys = new ArrayList<>(nameToPhone.keySet());
+		List<String> keys = new ArrayList<>(getNameToPhone().keySet());
 		keys.sort(new Comparator<String>() {
 
 			@Override
@@ -54,12 +80,12 @@ public class PhoneBook {
 			}
 		});
 		for (String key : keys) {
-			System.out.println(key + ", " + nameToPhone.get(key));
+			System.out.println(key + ", " + getNameToPhone().get(key));
 		}
 	}
 
 	public void printCallStatistic() {
-		Set<Entry<String, Integer>> set = phoneToCall.entrySet();
+		Set<Entry<String, Integer>> set = getPhoneToCall().entrySet();
 		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
 		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
 			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
@@ -113,8 +139,8 @@ public class PhoneBook {
 					String phone = split[1];
 					String phoneNumber = validatePhone(phone);
 					if (phoneNumber != null) {
-						nameToPhone.put(name, phoneNumber);
-						phoneToCall.put(phoneNumber, 0);
+						getNameToPhone().put(name, phoneNumber);
+						getPhoneToCall().put(phoneNumber, 0);
 						bw.write(name + SEPPARATOR + phoneNumber + "\r\n");
 						bw.flush();
 					}
@@ -127,7 +153,7 @@ public class PhoneBook {
 		}
 	}
 
-	private void addPhoneRecordToFile(String name, String number) {
+	public void addPhoneRecordToFile(String name, String number) {
 		File file = new File(parsedFilePath);
 
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
@@ -136,8 +162,8 @@ public class PhoneBook {
 			}
 			String phoneNumber = validatePhone(number);
 			if (phoneNumber != null) {
-				nameToPhone.put(name, phoneNumber);
-				phoneToCall.put(phoneNumber, 0);
+				getNameToPhone().put(name, phoneNumber);
+				getPhoneToCall().put(phoneNumber, 0);
 				bw.write(name + SEPPARATOR + phoneNumber + "\r\n");
 			}
 			bw.flush();
@@ -147,27 +173,26 @@ public class PhoneBook {
 		}
 	}
 
-	private void removePhoneRecord(String name) {
+	public void removePhoneRecord(String name) {
 		StringBuilder sb = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(new FileReader(new File(parsedFilePath)))
-				) {
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(parsedFilePath)))) {
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				String[] split = line.split(SEPPARATOR);
 				if (split[0].equalsIgnoreCase(name)) {
-					nameToPhone.remove(name);
-					phoneToCall.remove(split[1]);
+					getNameToPhone().remove(name);
+					getPhoneToCall().remove(split[1]);
 					continue;
 				}
 				sb.append(line);
 				sb.append("\r\n");
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File(parsedFilePath), false))){
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(parsedFilePath), false))) {
 			bw.write(sb.toString());
 			bw.flush();
 		} catch (IOException e) {
@@ -181,8 +206,8 @@ public class PhoneBook {
 	 * then if it is outgoing call it will call this method
 	 */
 	public void outgoingCall(String number) {
-		if (phoneToCall.containsKey(number)) {
-			phoneToCall.put(number, phoneToCall.get(number) + 1);
+		if (getPhoneToCall().containsKey(number)) {
+			getPhoneToCall().put(number, getPhoneToCall().get(number) + 1);
 		}
 	}
 
@@ -200,5 +225,21 @@ public class PhoneBook {
 
 	public void setParsedFile(String parsedFile) {
 		this.parsedFilePath = parsedFile;
+	}
+
+	public Map<String, String> getNameToPhone() {
+		return nameToPhone;
+	}
+
+	public void setNameToPhone(Map<String, String> nameToPhone) {
+		this.nameToPhone = nameToPhone;
+	}
+
+	public Map<String, Integer> getPhoneToCall() {
+		return phoneToCall;
+	}
+
+	public void setPhoneToCall(Map<String, Integer> phoneToCall) {
+		this.phoneToCall = phoneToCall;
 	}
 }
